806976264c8e2860a043a6f864d0bbb9ff007a94
Adam Wulf <adam@adamwulf.me>
2025-11-25 00:44:17 -0600

    Initial Commit

---
+++ Mantra/MantraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MantraApp.swift
+//  Mantra
+//
+//  Created by Adam Wulf on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MantraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
